61581cdee4e6d11ca45701bf1dc879bf5c66a1a1
EunJI <83203198+ejalice@users.noreply.github.com>
2022-07-15 22:29:37 +0900

    Create custom TableViewCell from the upcoming table and Create TitleViewModel

---
+++ Netflix Clone/Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by eunji on 2022/07/15.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    
+    let titleName: String
+    let posterURL: String
+}
