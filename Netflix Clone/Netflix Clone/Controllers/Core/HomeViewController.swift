ff8bc8ca6b66da36fe9915523e4732a2189b2108
EunJI <83203198+ejalice@users.noreply.github.com>
2022-07-17 00:54:48 +0900

    Create TitlePreviewViewController

--- Netflix Clone/Netflix Clone/Controllers/Core/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/Core/HomeViewController.swift
@@ -136,6 +136,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         
+        cell.delegate = self
+        
         switch indexPath.section {
         case Sections.TrendingMovies.rawValue:
             APICaller.shared.getTrendingMovies { result in
@@ -250,4 +252,14 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return sectionTitles[section]
     }
     
+}
+
+extension HomeViewController: CollectionViewTableViewCellDelegate {
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel) {
+        DispatchQueue.main.async { [weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
---
+++ Netflix Clone/Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by eunji on 2022/07/17.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
